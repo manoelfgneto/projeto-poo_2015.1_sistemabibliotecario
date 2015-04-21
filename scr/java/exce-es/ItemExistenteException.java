9825cb2562166e62ed8a6691154802d9557bde3b
Manoel Figueiroa Neto <manoelneto124@gmail.com>
2015-04-21 18:02:15 -0300

    Create ItemExistenteException.java

---
+++ scr/java/exce-es/ItemExistenteException.java
@@ -0,0 +1,13 @@
+package exceÃ§Ãµes;
+
+public class ItemExistenteException extends Exception {
+    
+     public ItemExistenteException(String message) {
+        super(message);
+    }
+
+    public ItemExistenteException(Throwable cause) {
+        super(cause);
+    }
+    
+}
