2770ba5f43ce3dda88017744368b78216b1fbb6b
Manoel Figueiroa Neto <manoelneto124@gmail.com>
2015-04-21 18:00:44 -0300

    Create ErroInternoException.java

---
+++ scr/java/exce-es/ErroInternoException.java
@@ -0,0 +1,13 @@
+package exceÃ§Ãµes;
+
+public class ErroInternoException extends Exception {
+    
+    public ErroInternoException(String message) {
+        super(message);
+    }
+
+    public ErroInternoException(Throwable cause) {
+        super(cause);
+    }
+    
+}
