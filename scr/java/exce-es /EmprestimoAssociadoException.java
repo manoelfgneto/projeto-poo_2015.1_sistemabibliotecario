c04dfbf5122dae81f96c67982c2f6582d08311ca
Manoel Figueiroa Neto <manoelneto124@gmail.com>
2015-04-21 17:59:33 -0300

    Create EmprestimoAssociadoException.java

---
+++ scr/java/exce-es /EmprestimoAssociadoException.java
@@ -0,0 +1,16 @@
+package exceÃ§Ãµes;
+
+public class EmprestimoAssociadoException extends Exception {
+
+    public EmprestimoAssociadoException(String message) {
+        super(message);
+    }
+
+    public EmprestimoAssociadoException(Throwable cause) {
+        super(cause);
+    }
+
+    public EmprestimoAssociadoException() {
+        super();
+    }
+}
